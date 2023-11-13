f2fa4d905c8d00f648aa41a8eda2c4deefd54cf2
Ahmed Magdy <65451360+ahmedmagdy9129@users.noreply.github.com>
2023-11-13 20:34:22 +0200

    added comments

--- index.js
+++ index.js
@@ -8,19 +8,24 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-const secretKey = "key123";
+// test secret key
+const secretKey = process.env.JWT_SECRET || "key123";
 
+// test GET / API
 app.get("/", (req, res) => {
   res.send("Welcome There!");
 });
 
 const users = {};
 
+// add user API
 app.post("/user", async (req, res) => {
   try {
     const payload = req.body;
+
     const salt = "450d0b0db2bcf4adde5032eca1a7c416e560cf44";
 
+    // before starting adding a new user, check if the user email already exists
     const existingUser = Object.values(users).find(
       (user) => user.email === payload.email
     );
@@ -32,11 +37,13 @@ app.post("/user", async (req, res) => {
       return;
     }
 
+    // hashing password using (email + the given salt)
     const hasedId = crypto
       .createHash("SHA1")
       .update(`${payload.email}${salt}`, 10)
       .digest("hex");
 
+    // creating an access token, we can add `expiresIn` prop for token expiration
     const accessToken = jwt.sign(hasedId, secretKey);
 
     const user = {
@@ -44,6 +51,7 @@ app.post("/user", async (req, res) => {
       ...payload,
     };
 
+    // append the new user to users object
     users[user.id] = user;
 
     res.status(200).json({ id: user.id, accessToken });
@@ -53,22 +61,28 @@ app.post("/user", async (req, res) => {
   }
 });
 
+// get user by id API
 app.get("/user/:id", (req, res) => {
   try {
     const { id } = req.params;
 
+    // check if the `id` is passed to the url or not
     if (!id) {
       res.status(400).json({
         error: "Invalid params",
       });
     }
+
+    // get the user object
     const user = users[id];
 
+    // check if user exists
     if (!user) {
       res.status(404).json({ error: "User not found" });
       return;
     }
 
+    // remove `email` prop when `marketingConsent` is false
     if (!user.marketingConsent) delete user.email;
 
     res.status(200).json(user);
